3581f97a66424c52f74d5561d0d861cddaecd129
KimYC1223 <kau_esc@naver.com>
2019-05-06 02:38:32 +0900

    VGA_GRAY sof and sopcinfo Update

---
+++ Quartus/SOPCINFO_FILE/software/vgaGray/obj/default/hello_world.d
@@ -0,0 +1,16 @@
+obj/default/hello_world.o: hello_world.c ../vgaGray_bsp/system.h \
+ ../vgaGray_bsp/linker.h ../vgaGray_bsp//HAL/inc/sys/alt_irq.h \
+ ../vgaGray_bsp//HAL/inc/nios2.h ../vgaGray_bsp//HAL/inc/alt_types.h \
+ ../vgaGray_bsp//HAL/inc/priv/alt_legacy_irq.h
+
+../vgaGray_bsp/system.h:
+
+../vgaGray_bsp/linker.h:
+
+../vgaGray_bsp//HAL/inc/sys/alt_irq.h:
+
+../vgaGray_bsp//HAL/inc/nios2.h:
+
+../vgaGray_bsp//HAL/inc/alt_types.h:
+
+../vgaGray_bsp//HAL/inc/priv/alt_legacy_irq.h:
--- software/save/obj/default/hello_world.d
+++
@@ -1,6 +0,0 @@
-obj/default/hello_world.o: hello_world.c ../save_bsp/system.h \
- ../save_bsp/linker.h
-
-../save_bsp/system.h:
-
-../save_bsp/linker.h:

b3ae6f2cc18c6d739db62f583b1e863c9854f7d2
KimYC1223 <kau_esc@naver.com>
2019-05-08 14:52:42 +0900

    DEFAULT_SETTING update  and MidTerm Exam Update

---
+++ Quartus/software/DEFAULT_SETTING/obj/default/hello_world.d
@@ -0,0 +1,7 @@
+obj/default/hello_world.o: hello_world.c \
+ C:/src/KAU_EMBEDED_19/Quartus/software/DEFAULT_SETTING_bsp/System.h \
+ C:/src/KAU_EMBEDED_19/Quartus/software/DEFAULT_SETTING_bsp/linker.h
+
+C:/src/KAU_EMBEDED_19/Quartus/software/DEFAULT_SETTING_bsp/System.h:
+
+C:/src/KAU_EMBEDED_19/Quartus/software/DEFAULT_SETTING_bsp/linker.h:
